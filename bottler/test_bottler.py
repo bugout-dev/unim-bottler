3bf6ba954ef50dd6be821252c253689de0887e73
Yhtyyar Sahatov <yhtiyarsahatow@gmail.com>
2022-01-28 16:26:58 +0300

    updated tests

--- bottler/test_bottler.py
+++ bottler/test_bottler.py
@@ -3,43 +3,32 @@ import unittest
 
 from brownie import accounts
 
-from . import BottlerFacet, BottlerInitializer
+
+from . import BottlerFacet
 from .core import facet_cut
-from .test_core import BottlerSingleContractTestCase, BottlerTestCase
+from .test_core import BottlerTestCase
+
+DECIMALS = 10 ** 18
 
 
-class TestDeployment(BottlerSingleContractTestCase):
-    def test_add_and_replace(self):
-        initializer = BottlerInitializer.BottlerInitializer(None)
-        initializer.deploy({"from": accounts[0]})
+class TestBottlerContract(BottlerTestCase):
+    def test_fill_small_bottles(self):
+        address = accounts[1].address
+        self.unim.mint(address, 500 * DECIMALS, {"from": accounts[0]})
+        self.unim.approve(self.bottler.address, 500 * DECIMALS, {"from": accounts[1]})
 
-        bottler_facet = BottlerFacet.BottlerFacet(None)
-        bottler_facet.deploy({"from": accounts[0]})
+        self.bottler.fill_small_bottles(1, {"from": accounts[1]})
 
-        diamond_address = self.contracts["Diamond"]
-        facet_cut(
-            diamond_address,
-            "BottlerFacet",
-            bottler_facet.address,
-            "add",
-            {"from": accounts[0]},
-            initializer.address,
-        )
+        self.assertEqual(self.unim.balance_of(address), 0)
+        self.assertEqual(self.bottler.get_full_bottle_inventory(address), [1, 0, 0])
 
-        diamond_bottler = BottlerFacet.BottlerFacet(diamond_address)
+    # def test_fill_small_bottles_with_insufficient_funds(self):
+    #     address = accounts[1].address
+    #     self.unim.mint(address, 500 * DECIMALS, {"from": accounts[1]})
+    #     self.unim.approve(self.bottler.address, 500 * DECIMALS, {"from": address})
 
-        controller = diamond_bottler.bottler_controller()
-        self.assertEqual(controller, accounts[0].address)
-
-
-class TestGetFullBottlePoolIds(BottlerTestCase):
-    def test_get_full_bottler_pool_ids(self):
-        bottler_diamond_address = self.bottler_contracts["Diamond"]
-        print(bottler_diamond_address)
-        diamond_bottler = BottlerFacet.BottlerFacet(bottler_diamond_address)
-
-        pool_ids = diamond_bottler.get_full_bottle_pool_ids()
-        self.assertEqual(pool_ids, [])
+    #     with self.assertRaises(Exception):
+    #         self.bottler.fill_small_bottles(2, {"from": accounts[1]})
 
 
 if __name__ == "__main__":
--- bottler/test_core.py
+++ bottler/test_core.py
@@ -1,21 +1,44 @@
+from typing import cast
 import unittest
 
 from brownie import accounts, network
 
+from bottler.BottlerFacet import BottlerFacet
+from bottler.OwnershipFacet import OwnershipFacet
+
 from .core import facet_cut, gogogo
-from . import BottlerFacet
-from . import BottlerInitializer
+
+from . import MockTerminus, MockErc20
 
 
 class BottlerSingleContractTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        print(accounts._accounts)
         try:
             network.connect()
         except:
             pass
-        cls.contracts = gogogo(accounts[0], {"from": accounts[0]})
+        cls.contracts = gogogo(accounts[0].address, {"from": accounts[0]})
+
+    def test_controller(self) -> None:
+        bottler = BottlerFacet(self.contracts["Diamond"])
+        ownership = OwnershipFacet(self.contracts["Diamond"])
+
+        self.assertEqual(ownership.owner(), accounts[0].address)
+        self.assertEqual(bottler.bottler_controller(), accounts[0].address)
+
+    def test_gogogo(self):
+        self.assertIn("DiamondCutFacet", self.contracts)
+        self.assertIn("Diamond", self.contracts)
+        self.assertIn("DiamondLoupeFacet", self.contracts)
+        self.assertIn("OwnershipFacet", self.contracts)
+        self.assertIn("attached", self.contracts)
+        self.assertIn("BottlerFacet", self.contracts)
+
+        self.assertListEqual(
+            self.contracts["attached"],
+            ["DiamondLoupeFacet", "OwnershipFacet", "BottlerFacet"],
+        )
 
 
 class BottlerTestCase(BottlerSingleContractTestCase):
@@ -23,41 +46,40 @@ class BottlerTestCase(BottlerSingleContractTestCase):
     def setUpClass(cls) -> None:
         super().setUpClass()
 
-        cls.bottler_contracts = gogogo(accounts[0], {"from": accounts[0]})
+        cls.terminus = MockTerminus.MockTerminus(None)
+        cls.terminus.deploy({"from": accounts[0]})
 
-        # Deploy Bottler
-        initializer = BottlerInitializer.BottlerInitializer(None)
-        initializer.deploy({"from": accounts[0]})
+        cls.unim = MockErc20.MockErc20(None)
+        cls.unim.deploy({"from": accounts[0]})
 
-        bottler_facet = BottlerFacet.BottlerFacet(None)
-        bottler_facet.deploy({"from": accounts[0]})
+        cls.mnstr = MockErc20.MockErc20(None)
+        cls.mnstr.deploy({"from": accounts[0]})
 
-        diamond_address = cls.contracts["Diamond"]
-        facet_cut(
-            diamond_address,
-            "BottlerFacet",
-            bottler_facet.address,
-            "add",
-            {"from": accounts[0]},
-            initializer.address,
-        )
+        cls.mnstr.mint(accounts[0], 10 ** 18, {"from": accounts[0]})
 
-        cls.bottler_initializer = initializer.address
-        cls.bottler_facet = bottler_facet.address
+        cls.terminus.set_payment_token(cls.mnstr.address, {"from": accounts[0]})
+        cls.terminus.set_pool_base_price(1, {"from": accounts[0]})
 
+        cls.mnstr.approve(cls.terminus.address, 10 ** 18, {"from": accounts[0]})
 
-class TestCoreDeployment(BottlerSingleContractTestCase):
-    def test_gogogo(self):
-        self.assertIn("DiamondCutFacet", self.contracts)
-        self.assertIn("Diamond", self.contracts)
-        self.assertIn("DiamondLoupeFacet", self.contracts)
-        self.assertIn("OwnershipFacet", self.contracts)
-        self.assertIn("attached", self.contracts)
+        bottler = BottlerFacet(cls.contracts["Diamond"])
+        bottler.set_up(cls.unim.address, cls.terminus.address, {"from": accounts[0]})
+        bottler.set_bottle_capacities([5000, 1000, 500], {"from": accounts[0]})
 
-        self.assertListEqual(
-            self.contracts["attached"],
-            ["DiamondLoupeFacet", "OwnershipFacet", "BottlerFacet"],
-        )
+        cls.bottler = bottler
+
+        for i in range(6):
+            cls.terminus.create_pool_v1(2 ** 256 - 1, True, True, {"from": accounts[0]})
+            cls.terminus.set_pool_controller(
+                i + 1, bottler.address, {"from": accounts[0]}
+            )
+
+        cls.pool_ids = {
+            "empty": [1, 2, 3],
+            "full": [4, 5, 6],
+        }
+        bottler.set_empty_bottle_pool_ids(cls.pool_ids["empty"], {"from": accounts[0]})
+        bottler.set_full_bottle_pool_ids(cls.pool_ids["full"], {"from": accounts[0]})
 
 
 if __name__ == "__main__":
