4c5f13ea5caffd64d34e384b187c32df93d0d155
Tim Pechersky <tim.pechersky@gmail.com>
2022-01-29 02:07:06 +0000

    web3 wont build without this

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,12 +3,18 @@ module.exports = {
   target: "serverless",
   trailingSlash: true,
   presets: [require.resolve("next/babel")],
-  webpack: (config, { isServer }) => {
+  webpack: (config, { isServer, webpack }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
       // config.node = { fs: 'empty' };
       config.resolve.fallback.fs = false;
+      config.resolve.fallback.electron = false;
     }
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^electron$/,
+      })
+    );
 
     return config;
   },
