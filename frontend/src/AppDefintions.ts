1a4e1c19fe2b1cc219ab0aad19315157b73d1ec1
Tim Pechersky <tim.pechersky@gmail.com>
2022-01-29 22:56:41 +0000

    changed to use env variables for addresses

--- frontend/src/AppDefintions.ts
+++ frontend/src/AppDefintions.ts
@@ -1,9 +1,2 @@
-export const UNIM_ADDRESS =
-  process.env.NODE_ENV === "development"
-    ? "0x3655B1500122aE4A0f1ADB85D6581F0127e63814"
-    : "NOT DEFINED YET!";
-
-export const BOTTLER_ADDRESS =
-  process.env.NODE_ENV === "development"
-    ? "0x5411aed2C143B3cdAD84c5BfeC908F9AF136B97D"
-    : "NOT DEFINED YET!";
+export const MILK_ADDRESS = process.env.NEXT_PUBLIC_UNIMLK_ADDRESS;
+export const BOTTLER_ADDRESS = process.env.NEXT_PUBLIC_BOTTLER_ADDRESS;
